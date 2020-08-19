e3245c841ae2c2ff635716d81c9bd3d505369e65
Robert Heath <robert.b.heathii@gmail.com>
2020-08-19 15:27:17 -0500

    Graded Commit

--- lib/SchoolTest.rb
+++ lib/SchoolTest.rb
@@ -1,8 +1,7 @@
 class SchoolTest
+
   def initialize(start_time, hours_in_school_day)
     @start_time = start_time
     @hours_in_school_day = hours_in_school_day
   end
 end
-
-school = School.new('9:00', 7)
